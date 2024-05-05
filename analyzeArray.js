e1f707a72e47333f17250131618758bba73d5c6f
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2024-05-05 11:42:38 -0400

    Added tests for passing non array and empty array for analyzeArray function.

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,4 +1,18 @@
 function analyzeArray(arr) {
+    // IF input is not an array throw an error
+    if (!Array.isArray(arr)) {
+        throw new Error("Input must be an array");
+    }
+
+    if (arr.length === 0) {
+        return {
+            average: NaN,
+            min: NaN,
+            max: NaN,
+            length: 0
+        }
+    }
+
     const sum = arr.reduce((total, val) => total + val, 0)
     const average = sum / arr.length
     const min = Math.min(...arr)
--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -16,4 +16,15 @@ describe('Analyze  Array', () => {
     test('should take an array of numbers and return an object with an "length" property.', () => {
         expect(analyzeArray([1, 2, 3, 4])).toHaveProperty("length");
     });
+
+    test('should throw error if the input is not an array.', () => {
+        expect(() => mockFunction(123)).toThrowError();
+    });
+
+    test('on an empty array, should return NaN.', () => {
+        expect(isNaN(analyzeArray([]).average)).toBeTruthy();
+        expect(isNaN(analyzeArray([]).min)).toBeTruthy();
+        expect(isNaN(analyzeArray([]).max)).toBeTruthy();
+        expect(analyzeArray([]).length).toEqual(0);
+    });
 });
