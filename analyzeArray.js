8f958872ac579b32b489cf5e081850e313c90ff1
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2024-05-04 21:58:25 -0400

    Added to function to satisfy inital test cases for analyzeArray function.

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,4 +1,14 @@
-function analyzeArray() {
+function analyzeArray(arr) {
+    const sum = arr.reduce((total, val) => total + val, 0)
+    const average = sum / arr.length
+    const min = Math.min(...arr)
+    const max = Math.max(...arr)
 
+    return {
+        average,
+        min,
+        max,
+        length: arr.length
+    }
 }
 export default analyzeArray
